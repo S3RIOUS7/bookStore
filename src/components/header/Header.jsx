e28f29b68bec8997f0071a016cece2c096d0af74
S3RIOUS7 <107929484+S3RIOUS7@users.noreply.github.com>
2023-10-20 12:54:35 +0300

    last commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,49 +1,57 @@
-
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setSelectedOrderBy,
+  setSelectedCategory,
+  setBooks,
+  setTotalBooks,
+  setSearchInput,
+} from "../../redux/actions";
 import Input from "../input/Input";
 import PopUpMenu from "../popUpMenu/PopUpMenu";
-import './header.scss';
 import { options1, options2 } from '../../utils/constants/constants.js';
-import { fetchBySearchBook } from "../../api/fetchBySearchBook";
+import { fetchBySearchBook } from '../../api/fetchBySearchBook'
+import '../header/header.scss'
 
-function Header({ setBooks, setTotalBooks, searchInput, setSearchInput, selectedCategory, selectedOrderBy, setSelectedCategory, setSelectedOrderBy }) {
-  
+function Header() {
+  const selectedOrderBy = useSelector((state) => state.selectedOrderBy);
+  const selectedCategory = useSelector((state) => state.selectedCategory);
+  const searchInput = useSelector((state) => state.searchInput);
 
+  const dispatch = useDispatch();
 
   const handleSearch = async () => {
-    
-  const res=await fetchBySearchBook( searchInput, 10, selectedOrderBy, selectedCategory);
-  setBooks(res.items)
-  setTotalBooks(res.totalItems);  
-    
+    const res = await fetchBySearchBook(searchInput, 10, selectedOrderBy, selectedCategory);
+    dispatch(setBooks(res.items));
+    dispatch(setTotalBooks(res.totalItems));
   };
- 
-  
+
   return (
     <div className="header">
       <div className="headerContainer">
         <h2>Search for the books</h2>
         <Input
-        label="Search Books"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-        type="text"
-      />
+          label="Search Books"
+          value={searchInput}
+          onChange={(e) => dispatch(setSearchInput(e.target.value))}
+          type="text"
+        />
         <div className="popDiscription">
-          <h3>Categories</h3> <PopUpMenu options={options1}
-           selectedOption={selectedCategory}
-           setSelectedOption={setSelectedCategory}
+          <h3>Categories</h3>
+          <PopUpMenu
+            options={options1}
+            selectedOption={selectedCategory}
+            setSelectedOption={(option) => dispatch(setSelectedCategory(option))}
           />
-          <h3>Sorting by</h3> <PopUpMenu
-              options={options2}
-              selectedOption={selectedOrderBy}
-              setSelectedOption={setSelectedOrderBy}
-            />
+          <h3>Sorting by</h3>
+          <PopUpMenu
+            options={options2}
+            selectedOption={selectedOrderBy}
+            setSelectedOption={(option) => dispatch(setSelectedOrderBy(option))}
+          />
         </div>
-        <button onClick={handleSearch}>
-          Search
-        </button>
+        <button onClick={handleSearch}>Search</button>
       </div>
-     
     </div>
   );
 }
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,15 @@
 import React, { Fragment } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setBooks } from "../actions/appActions";
+import { setBooks } from "../../redux/actions";
 import { fetchBySearchBook } from "../../api/fetchBySearchBook";
-
+import  CardBook  from '../cardbook/CardBook'
+import '../main/main.scss'
 function Main() {
   const books = useSelector((state) => state.books);
   const totalBooks = useSelector((state) => state.totalBooks);
   const selectedCategory = useSelector((state) => state.selectedCategory);
   const selectedOrderBy = useSelector((state) => state.selectedOrderBy);
-
+  const searchInput = useSelector((state) => state.searchInput);
   const dispatch = useDispatch();
 
   const loadBooks = async () => {
@@ -18,11 +19,12 @@ function Main() {
       selectedOrderBy,
       selectedCategory
     );
-    dispatch(setBooks([...books, ...res.items])
-  };
+    dispatch(setBooks([...books, ...res.items]))
+  }
 
   return (
     <Fragment>
+
       <h2>Books ({totalBooks})</h2>
       <div className="main-container">
         {books.map((book) => (
