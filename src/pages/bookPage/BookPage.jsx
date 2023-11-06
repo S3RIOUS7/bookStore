fc0f26c657894b285cf4ec823a1657c159dbf8fc
S3RIOUS7 <107929484+S3RIOUS7@users.noreply.github.com>
2023-11-06 15:21:31 +0200

    last commit

--- src/pages/bookPage/BookPage.jsx
+++ src/pages/bookPage/BookPage.jsx
@@ -1,24 +1,31 @@
 import Header from "../../components/header/Header";
 import { Fragment } from "react";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import '../bookPage/bookPage.scss'
 function BookPage () {
   const { bookId } = useParams();
   const books = useSelector((state) => state.books);
   const selectedBook = books.find((book) => book.id === bookId);
-  
-  
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1); 
+  };
+
   return(
     <Fragment>
       <Header />
-      <div>
-      <h1>{selectedBook.volumeInfo.title}</h1>
-      <p>Автор: {selectedBook.volumeInfo.authors}</p>
-      <img src={selectedBook.volumeInfo.imageLinks.thumbnail} alt="Book Cover" />
-      <p>{selectedBook.volumeInfo.description}</p>
-    </div>
+      <div className="bookPage-container">
+      <div className="imgContainer"><img src={selectedBook?.volumeInfo?.imageLinks?.thumbnail} className="imgBookPage" alt="Book Cover" /></div>
+      <div className="bookPageInfo">
+        <h1>{selectedBook?.volumeInfo?.title}</h1>
+      <p>Автор: {selectedBook?.volumeInfo?.authors}</p>
+      <p>Описание : {selectedBook?.volumeInfo?.description}</p>
+      </div>
+      </div>
+      <button className="bookPageButton" onClick={goBack}>Back</button>
+    
     </Fragment>
   )
 
